1378fe3d964371ae010c45cc0e7c61e485914282
Artur <exstrominer@gmail.com>
2017-07-15 15:45:29 +0300

    Hello World && A + B

---
+++ app.js
@@ -0,0 +1,3 @@
+const skb = new (require('skb'))('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1OTVmZTI5NDdkMWEzMTAwMTFhMjFjNmEiLCJ1c2VybmFtZSI6ImV4c3Ryb21pbmVyQGdtYWlsLmNvbSIsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNDk5NDU2MjQ2fQ.dyHLkwQNxt0jrezFHLl06P0le1FBpackZyszEYErY78');
+
+skb.taskHelloWorld('Hello World');
---
+++ summ.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var cors = require('cors');
+var app = express();
+
+app.use(cors())
+
+app.get('/summ', (req, res) => {
+    let { a, b } = req.query;
+
+    console.log(`New request - ${JSON.stringify(req.query)}`);
+
+    if (!a && !b) {
+        if (req.query.some)
+            return res.send(req.query.some);
+
+        return res.send('0');
+    }
+
+    if (!a) return res.send(b);
+
+    if (!b) return res.send(a);
+
+    let summ = ((+a) + (+b)).toString();
+
+    res.send(summ);
+});
+
+app.listen(80, () => console.log('CORS-enabled web server listening on port 80'));
